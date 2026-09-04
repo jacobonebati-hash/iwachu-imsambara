3de5e276d3ca3fcf332521907491dd48d5ad11ab
Nebati Jacobo <jacobonebati@gmail.com>
2026-09-04 23:54:03 +0300

    Configure Django for Render deployment

--- iwachu_system/settings.py
+++ iwachu_system/settings.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import os
 
 from dotenv import load_dotenv
-
+import dj_database_url
 
 # =========================================================
 # BASE DIRECTORY
@@ -35,9 +35,14 @@ SECRET_KEY = os.getenv(
 )
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
+
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
+    ".onrender.com",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com",
 ]
 
 if os.getenv("RENDER_EXTERNAL_HOSTNAME"):
@@ -210,19 +215,11 @@ WSGI_APPLICATION = 'iwachu_system.wsgi.application'
 # =========================================================
 
 DATABASES = {
-
-    'default': {
-
-        'ENGINE':
-            'django.db.backends.sqlite3',
-
-        'NAME':
-            BASE_DIR / 'db.sqlite3',
-
-    }
-
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
-
 
 # =========================================================
 # PASSWORD VALIDATION
